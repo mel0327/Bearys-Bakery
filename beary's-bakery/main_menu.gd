7a93e73a913d443261aec30cf02a4def9f2f9e07
TiffyZeng46 <159198240+TiffyZeng46@users.noreply.github.com>
2025-03-26 10:58:06 -0700

    Fixed bugs for the start menu and options menu

--- beary's-bakery/main_menu.gd
+++ beary's-bakery/main_menu.gd
@@ -4,6 +4,8 @@ extends Control
 @onready var start_button = %StartButton as Button
 @onready var exit_button = %ExitButton as Button
 @onready var options_button = %OptionsButton as Button
+@onready var options_menu = $OptionsMenu as OptionsMenu
+@onready var margin_container = $MarginContainer as MarginContainer
 @onready var start_level = preload("res://test.tscn")
 
 func _ready() -> void:
@@ -14,14 +16,22 @@ func on_start_button_pressed() -> void:
 
 
 func on_options_button_pressed() -> void:
-	print("settings")
+	margin_container.visible = false
+	options_menu.set_process(true)
+	options_menu.visible = true
 
 
 func on_exit_button_pressed() -> void:
 	get_tree().quit()
+	
+func on_exit_options_menu() -> void:
+	margin_container.visible = true
+	options_menu.visible = false
+
 
 func handle_connecting_signals() -> void:
 	start_button.button_down.connect(on_start_button_pressed)
 	options_button.button_down.connect(on_options_button_pressed)
 	exit_button.button_down.connect(on_exit_button_pressed)
+	options_menu.exit_options_menu.connect(on_exit_options_menu)
 	
