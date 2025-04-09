3062f11ef9f652cd443caa34d7bde99ddb0760ae
TiffyZeng46 <159198240+TiffyZeng46@users.noreply.github.com>
2025-04-09 11:13:27 -0700

    Created speed pickup

--- beary's-bakery/item_speed.gd
+++ beary's-bakery/item_speed.gd
@@ -1,7 +1,7 @@
-class_name ItemSpeed extends Item
+class_name ItemSpeedBoost extends Item
 
-@export var speed_amount := 1
 
+@export var speed_amount := 1000
 
 func use(player: Player) -> void:
 	player.speed += speed_amount
