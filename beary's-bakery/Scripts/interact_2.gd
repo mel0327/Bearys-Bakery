3ff02ce72e2efd38bfbc306f5adef10c8f76a1eb
Melissa <meho@s.sfusd.edu>
2025-04-18 14:17:50 -0700

    Day 1

---
+++ beary's-bakery/Scripts/interact_2.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+
+@onready var play_dialogue = preload("res://Scenes/dialogue_2.1.tscn")
+
+var is_player_near := false
+
+
+func _ready() -> void:
+	body_entered.connect(func (body: Node) -> void:
+		is_player_near = true
+	)
+	body_exited.connect(func (body: Node) -> void:
+		is_player_near = false
+	)
+
+
+func _physics_process(_delta: float) -> void:
+	if is_player_near and Input.is_action_just_pressed("interact"):
+		var dialogue_instance = play_dialogue.instantiate()
+		get_parent().add_child(dialogue_instance)
+		set_deferred("monitoring", false)
