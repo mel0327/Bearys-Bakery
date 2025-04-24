6ea94548a8e970e00774fd466f07c1567c69dcb5
Melissa <meho@s.sfusd.edu>
2025-04-24 09:39:09 -0700

    pause and resume player movements

--- beary's-bakery/Scripts/dialogue_2.gd
+++ beary's-bakery/Scripts/dialogue_2.gd
@@ -1,4 +1,14 @@
 extends Node2D
 
+signal dialogue_finished
+
 func _ready() -> void:
+	Dialogic.timeline_ended.connect(_on_timeline_ended)
 	Dialogic.start("opening")
+
+func _on_timeline_ended() -> void:
+	end_dialogue()
+
+func end_dialogue():
+	emit_signal("dialogue_finished")
+	queue_free()
