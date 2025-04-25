c233399f7fac97f4b29dfb94ce60673d8af84339
TiffyZeng46 <159198240+TiffyZeng46@users.noreply.github.com>
2025-04-25 11:17:31 -0700

    Fixed Hammy to detect the player

--- beary's-bakery/Scripts/mob.gd
+++ beary's-bakery/Scripts/mob.gd
@@ -15,19 +15,9 @@ var _player: Player = null
 
 
 func _ready() -> void:
-	_detection_area.body_entered.connect(func (body: Node) -> void:
-		if body is Player:
-			_player = body
-	)
-	_detection_area.body_exited.connect(func (body: Node) -> void:
-		if body is Player:
-			_player = null
-	)
-
-	_hit_box.body_entered.connect(func (body: Node) -> void:
-		if body is Player:
-			body.health -= damage
-	)
+	_detection_area.body_entered.connect(_on_detection_area_body_entered)
+	_detection_area.body_exited.connect(_on_detection_area_body_exited)
+	_hit_box.body_entered.connect(_on_hit_box_body_entered)
 
 
 func set_health(new_health: int) -> void:
@@ -65,3 +55,23 @@ func _physics_process(delta: float) -> void:
 
 func take_damage(amount:int) -> void:
 	set_health(health-amount)
+
+
+func _on_hit_box_body_entered(body: Node2D) -> void:
+	if body is Player:
+		body.health -= damage
+	elif body is Bullet:
+		take_damage(body.damage)
+		body.queue_free()
+
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	if body is Player:
+		print(get_class(), "detected the player!")
+		_player = body
+
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	if body is Player:
+		print(get_class(), "lost the player!")
+		_player = body
