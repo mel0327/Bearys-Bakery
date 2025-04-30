c8e8c7609b0f5aad7017885f95cd793f2153a76c
TiffyZeng46 <159198240+TiffyZeng46@users.noreply.github.com>
2025-04-30 11:27:20 -0700

    Created animation for player

--- beary's-bakery/Scripts/player.gd
+++ beary's-bakery/Scripts/player.gd
@@ -9,7 +9,7 @@ var can_move = true
 
 @onready var health_bar: ProgressBar = %HealthBar
 @onready var collision_shape_2d: CollisionShape2D = %CollisionShape2D
-
+@onready var animated_sprite: AnimatedSprite2D = %AnimatedSprite2D
 
 func _ready() -> void:
 	health_bar.max_value = 4
@@ -18,6 +18,7 @@ func _ready() -> void:
 func _physics_process(delta):
 	if can_move:
 		handle_movement(delta)
+		update_animation()
 
 func handle_movement(delta: float) -> void:
 
@@ -42,3 +43,33 @@ func pause_movement():
 
 func resume_movement():
 	can_move = true
+
+func update_animation() -> void:
+	var last_motion = get_last_motion()
+	if last_motion.x == 0 and last_motion.y == 0:
+		if animated_sprite.animation != "idle":
+			animated_sprite.play("idle")
+		return
+	if (abs(last_motion.x) > abs(last_motion.y)):	
+		if last_motion.x > 0:
+			#print ("right")
+			animated_sprite.flip_h = true
+			if animated_sprite.animation != "right":
+				animated_sprite.play("right")
+		elif last_motion.x < 0:
+			#print("left")
+			animated_sprite.flip_h = true
+			if animated_sprite.animation != "left":
+				animated_sprite.play("left")
+	else:
+		if last_motion.y > 0:
+			#print ("Up")
+			animated_sprite.flip_h = true
+			if animated_sprite.animation != "front":
+				animated_sprite.play("front")
+		elif last_motion.y < 0:
+			#print("back")
+			animated_sprite.flip_h = true
+			if animated_sprite.animation != "back":
+				animated_sprite.play("back")
+	
