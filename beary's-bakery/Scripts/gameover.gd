bdda2907851f488d7db12f670e3f9c2a93781d51
ellawchen <elchen14@s.sfusd.edu>
2025-05-16 11:15:43 -0700

    restart/quit button w/ background

--- beary's-bakery/Scripts/gameover.gd
+++ beary's-bakery/Scripts/gameover.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
  
 
 func _on_restart_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://test.tscn")
+	get_tree().change_scene_to_file("res://Scenes/bakery.tscn")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
